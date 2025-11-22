2b690da34bd7c623e65ee61523b493dc8afed6f0
mariobellot-stack <mariobellot@sbcglobal.net>
2025-11-22 08:52:48 -0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => (
+  <div style={{ textAlign: 'center', marginTop: '20%' }}>
+    <h1>ProFlow Services v1.2</h1>
+    <p>Professional Pool Maintenance & Filtration Specialists</p>
+  </div>
+);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
